86d4dcaff55ae2302f44ed6cacee6cadcca7c094
HoangPhiHong1 <hphoang@knox.edu>
2024-04-15 22:52:00 -0500

    connect to mongodb environment

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -2,3 +2,27 @@
 // mHLTL1orpZVn57Ve
 
 const MONGODB_URI = 'mongodb+srv://1hoangphihong1:mHLTL1orpZVn57Ve@clustertrelloclone.jqlhtlg.mongodb.net/?retryWrites=true&w=majority'
+const DATABASE_NAME = 'trello-db'
+
+import { MongoClient, ServerApiVersion } from 'mongodb'
+
+let trelloDatabaseInstance = null
+
+const mongoClientInstance = new MongoClient(MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict:true,
+    deprecationErrors: true
+  }
+})
+
+export const CONNECT_DB = async () => {
+  await mongoClientInstance.connect()
+
+  trelloDatabaseInstance = mongoClientInstance.db(DATABASE_NAME)
+}
+
+export const GET_DB = () => {
+  if (!trelloDatabaseInstance) throw new Error('Must connect to Database first')
+  return trelloDatabaseInstance
+}

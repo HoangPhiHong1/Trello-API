c5a83d374afe296efac8eb7d9e7359cde52d7344
HoangPhiHong1 <hphoang@knox.edu>
2024-04-16 20:42:20 -0500

    add exit hook

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 import express from 'express'
-import { CONNECT_DB, GET_DB } from '~/config/mongodb'
+import exitHook from 'async-exit-hook'
+import { CONNECT_DB, GET_DB, CLOSE_DB } from '~/config/mongodb'
 
 const START_SERVER = () => {
   const app = express()
@@ -19,7 +20,9 @@ const START_SERVER = () => {
     // eslint-disable-next-line no-console
     //console.log('Please read script in package json file')
   })
-
+  exitHook(() => {
+    CLOSE_DB()
+  })
 }
 
 (async () => {
